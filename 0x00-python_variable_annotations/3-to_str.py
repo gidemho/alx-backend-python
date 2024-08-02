84fe569d450737c5a7dd3fcc91df4243316a1d1b
gidemho <gidemho@gmail.com>
2024-08-02 05:29:08 -0700

    3-to_str.py

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to convert a float number to its string rep.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Returns the string rep of a float number.
+
+    Parameters:
+    n (float): The number to convert to a string.
+
+    Returns:
+    str: The string rep of the number.
+    """
+    return str(n)
